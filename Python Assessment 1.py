e81a90e5fe14cda040e34c713ce8b0e453305ad1
GsProjects <gearoidlacey@gmail.com>
2017-10-27 11:34:32 +0100

    Further cleaning to DBA Name and AKA Name

--- Python Assessment 1.py
+++ Python Assessment 1.py
@@ -5,8 +5,8 @@ food = list(csv.DictReader(open('Temporary.csv')))
 column = input('Enter the column you want to process or enter nothing to escape: ')
 if column != '':
     temporary_data = {row[column] for row in food}
-    print(temporary_data)
     print(len(temporary_data))
+
 
 def remove_duplicate_locations(data):
     print('In remove_duplicate_locations')
@@ -29,6 +29,7 @@ def remove_duplicate_locations(data):
         del item['Latitude']
         del item['Longitude']
         new_data.append(item)
+    print(len(new_data))
     return new_data
 
 
@@ -48,6 +49,7 @@ def ammend_AKA(data):
             item['DBA Name'] = new_name
             item['AKA Name'] = new_name
         new_data.append(item)
+    print(len(new_data))
     return new_data
 
 
@@ -55,7 +57,7 @@ def clean_text_data(data):
     print('In clean_text_data')
     result = ammend_city(data)
     new_data = []
-    text_based_columns = ['DBA Name', 'Inspection Type']
+    text_based_columns = ['DBA Name', 'AKA Name', 'Inspection Type']
     for x in result:
         for value in x:
             x[value] = x[value].lower()
@@ -66,9 +68,11 @@ def clean_text_data(data):
                 x[item] = x[item].replace(",", "")
                 x[item] = x[item].replace("-", " ")
                 x[item] = x[item].replace("/", " ")
+                x[item] = x[item].lstrip(" ")
+                x[item] = x[item].rstrip(" ")
 
-        new_data.append(x)
-    return new_data
+    print(len(result))
+    return result
 
 
 def ammend_city(data):
@@ -84,28 +88,48 @@ def ammend_city(data):
 
     for item in new_data:  # following this delete the city column completely
         del item['City']
-
+    print(len(new_data))
     return new_data
 
 
 def remove_state(data):
     print('In remove_state')
+    result = change_date_format(data)
     new_data = []
-    for x in data:
+    for x in result:
         del x['State']
         new_data.append(x)
+    print(len(new_data))
+    return new_data
+
+
+def change_date_format(data):
+    print('In change_date_format')
+    new_data = []
+    for x in data:
+        temporary_date = x['Inspection Date'].split('/')
+        new_date = temporary_date[1] + '/' + temporary_date[0] + '/' + temporary_date[2]
+        x['Inspection Date'] = new_date
+        new_data.append(x)
+    print(len(new_data))
     return new_data
 
 
 def write_data(data):
     with open('TemporaryOutput.csv', "w") as fh:
-        out_csv = csv.DictWriter(fh,list(data[0].keys()))
+        out_csv = csv.DictWriter(fh, list(data[0].keys()))
         out_csv.writeheader()
         out_csv.writerows(data)
 
 
 result = remove_duplicate_locations(food)
-temporary = {row[column] for row in result}
-print(temporary)
-print(len(temporary))
-#write_data(result)
+#temporary = Counter(row[column] for row in result)
+
+#temporary = {row[column] for row in result}
+#print(len(temporary))
+
+#print(temporary.most_common(50))
+'''a = temporary.most_common(10)
+for item in a:
+    print(len(item[0]))'''
+write_data(result)
