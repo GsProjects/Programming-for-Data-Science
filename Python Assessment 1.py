67491d9b7c1c6da7754d3cf0161029271ba9b061
GsProjects <gearoidlacey@gmail.com>
2017-10-29 17:27:11 +0000

    Removing check for common elements in DBA and AKA Name

--- Python Assessment 1.py
+++ Python Assessment 1.py
@@ -8,9 +8,19 @@ if column != '':
     print(len(temporary_data))
 
 
-def remove_duplicate_locations(data):
+def fill_cells(data):  # replace empty cells with null value
+    print('In change_date_format')
+    result = remove_duplicate_locations(data)
+    for x in result:
+        for value in x.keys():
+            if x[value] == '':
+                x[value] = 'NULL'
+    return data
+
+
+def remove_duplicate_locations(data):  # populate empty locations, then remove longitude and latitude columns
     print('In remove_duplicate_locations')
-    result = alter_names(data)
+    result = clean_text_data(data)
     temp_data = []
     location = set()
     #  If location col empty populate with longitude and latitude
@@ -32,26 +42,8 @@ def remove_duplicate_locations(data):
     print(len(new_data))
     return new_data
 
-#  GETTING COMMON ELEMENTS FROM DBA AND AKA NAMES
-#  THEN SETTING THE COMMON ELEMENTS AS THE VALUES
-def alter_names(data):
-    print('In ammend AKA')
-    result = clean_text_data(data)
-    for item in result:
-        dba_name = item['DBA Name'].split()
-        aka_name = set(item['AKA Name'].split())
-        common = [x for x in dba_name if
-                  x in aka_name]  # this comprehension will maintain the order of the common elements
-        new_name = ''
-        if len(common) != 0:  # if there is common elements
-            for items in common:
-                new_name = new_name + ' ' + items
-            item['DBA Name'] = new_name
-            item['AKA Name'] = new_name
-    return result
 
-
-def clean_text_data(data):
+def clean_text_data(data):  # remove punctuation and whitespace to prevent duplicates
     print('In clean_text_data')
     result = ammend_city(data)
     text_based_columns = ['DBA Name', 'AKA Name', 'Inspection Type']
@@ -63,8 +55,8 @@ def clean_text_data(data):
                 x[item] = x[item].replace("'", "")
                 x[item] = x[item].replace(".", "")
                 x[item] = x[item].replace(",", "")
-                x[item] = x[item].replace("-", " ")
-                x[item] = x[item].replace("/", " ")
+                #x[item] = x[item].replace("-", " ")
+                #x[item] = x[item].replace("/", " ")
                 x[item] = x[item].lstrip(" ")
                 x[item] = x[item].rstrip(" ")
 
@@ -72,7 +64,7 @@ def clean_text_data(data):
     return result
 
 
-def ammend_city(data):
+def ammend_city(data):  # if city value does not contain chicago substring remove the row, then remove the column
     print('In ammend_city')
     result = remove_state(data)
     new_data = []
@@ -97,9 +89,9 @@ def remove_state(data):
     return result
 
 
-def change_date_format(data):
+def change_date_format(data):  # changes date from mm/dd/yy to dd/mm/yy
     print('In change_date_format')
-    result = fill_cells(data)
+    result = fill_names(data)
     for x in result:
         temporary_date = x['Inspection Date'].split('/')
         new_date = temporary_date[1] + '/' + temporary_date[0] + '/' + temporary_date[2]
@@ -107,17 +99,7 @@ def change_date_format(data):
     return result
 
 
-def fill_cells(data):
-    print('In change_date_format')
-    result = fill_names(data)
-    for x in result:
-        for value in x.keys():
-            if x[value] == '':
-                x[value] = 'NULL'
-    return data
-
-
-def fill_names(data):
+def fill_names(data):  # populate empty name cells if possible
     print('In change_date_format')
     for x in data:
         if x['DBA Name'] == '':
@@ -135,10 +117,10 @@ def write_data(data):
 
 
 result = remove_duplicate_locations(food)
-#temporary = Counter(row[column] for row in result)
+temporary = Counter(row[column] for row in result)
 
 #temporary = {row[column] for row in result}
-#print(len(temporary))
+print(len(temporary))
 
 #print(temporary.most_common(50))
 '''a = temporary.most_common(10)
