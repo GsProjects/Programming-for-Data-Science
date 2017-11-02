9f4c511a72fc8749792964e6a4c61564b815f925
GsProjects <gearoidlacey@gmail.com>
2017-11-02 23:36:25 +0000

    No longer removes rows based on city value

--- Python Assessment 1.py
+++ Python Assessment 1.py
@@ -1,22 +1,32 @@
 import csv
 from collections import Counter
 
-food = list(csv.DictReader(open('Temporary.csv')))
+food = list(csv.DictReader(open('Food_Inspections.csv')))
 column = input('Enter the column you want to process or enter nothing to escape: ')
 if column != '':
     temporary_data = {row[column] for row in food}
+    print(temporary_data)
     print(len(temporary_data))
 
 
 def fill_cells(data):  # replace empty cells with null value
     print('In change_date_format')
-    result = remove_duplicate_locations(data)
+    result = ammend_inspections(data)
     for x in result:
         for value in x.keys():
             if x[value] == '':
                 x[value] = 'NULL'
-    return data
+    print('Returning 6')
+    return result
 
+def ammend_inspections(data):# clean inspection type data
+    print('In ammend_inspections')
+    result = remove_duplicate_locations(data)
+    for items in result:
+        items['Inspection Type'] = items['Inspection Type'].replace('canvas', 'canvass')
+        items['Inspection Type'] = items['Inspection Type'].replace('out ofbusiness', 'out of business')
+    return result
+                                                                             
 
 def remove_duplicate_locations(data):  # populate empty locations, then remove longitude and latitude columns
     print('In remove_duplicate_locations')
@@ -36,10 +46,10 @@ def remove_duplicate_locations(data):  # populate empty locations, then remove l
     #  Remove longitude and latitude columns
     new_data = []
     for item in temp_data:
-        del item['Latitude']
-        del item['Longitude']
+        item.pop('Latitude')
+        item.pop('Longitude')
         new_data.append(item)
-    print(len(new_data))
+    print('Returning 5')
     return new_data
 
 
@@ -55,12 +65,11 @@ def clean_text_data(data):  # remove punctuation and whitespace to prevent dupli
                 x[item] = x[item].replace("'", "")
                 x[item] = x[item].replace(".", "")
                 x[item] = x[item].replace(",", "")
-                #x[item] = x[item].replace("-", " ")
-                #x[item] = x[item].replace("/", " ")
+                x[item] = x[item].replace("-", " ")
+                x[item] = x[item].replace("/", " ")
                 x[item] = x[item].lstrip(" ")
                 x[item] = x[item].rstrip(" ")
-
-    print(len(result))
+    print('Returning 4')
     return result
 
 
@@ -70,22 +79,22 @@ def ammend_city(data):  # if city value does not contain chicago substring remov
     new_data = []
     city = 'chicago'
     for x in result:
-        if city in x['City'].lower():
+        if city in x['City'].lower() or x['City'].lower() == '':
             new_data.append(x)
+        elif x['City'].lower() == 'chcicago':
+            x['City'] = city
         else:
-            result.remove(x)  # if city is not chicago delete the row
-
-    for item in new_data:  # following this delete the city column completely
-        del item['City']
-    print(len(new_data))
+            x['City'] = 'non-chicago address, attention required'
     return new_data
 
 
 def remove_state(data):
     print('In remove_state')
     result = change_date_format(data)
+    
     for x in result:
-        del x['State']
+        x.pop('State')
+    print('Returning 2')
     return result
 
 
@@ -93,21 +102,32 @@ def change_date_format(data):  # changes date from mm/dd/yy to dd/mm/yy
     print('In change_date_format')
     result = fill_names(data)
     for x in result:
-        temporary_date = x['Inspection Date'].split('/')
-        new_date = temporary_date[1] + '/' + temporary_date[0] + '/' + temporary_date[2]
-        x['Inspection Date'] = new_date
+        if x['Inspection Date'] != '':
+            x['Inspection Date'] = x['Inspection Date'].replace('-', '/')
+            temporary_date = x['Inspection Date'].split('/')
+            new_date = temporary_date[1] + '/' + temporary_date[0] + '/' + temporary_date[2]
+            x['Inspection Date'] = new_date
+    print('Returning 1')
     return result
 
 
 def fill_names(data):  # populate empty name cells if possible
     print('In change_date_format')
-    for x in data:
+    result = change_case(data)
+    for x in result:
         if x['DBA Name'] == '':
             x['DBA Name'] = x['AKA Name']
         elif x['AKA Name'] == '':
             x['AKA Name'] = x['DBA Name']
     return data
 
+
+def change_case(data):
+    for item in data:
+        for value in item:
+            item[value] = item[value].lower()
+    return data        
+    
 
 def write_data(data):
     with open('TemporaryOutput.csv', "w") as fh:
@@ -116,17 +136,20 @@ def write_data(data):
         out_csv.writerows(data)
 
 
-result = remove_duplicate_locations(food)
-temporary = Counter(row[column] for row in result)
+result = fill_cells(food)
 
-#temporary = {row[column] for row in result}
+#temporary = Counter(row[column] for row in result)
+
+temporary = {row[column] for row in result}
+print(temporary)
 print(len(temporary))
+'''
+#print(Counter(temporary))
+#with open('Temp_Result.txt', 'w') as file:
+#    file.write(str(Counter(temporary)))
 
 #print(temporary.most_common(50))
-'''a = temporary.most_common(10)
+a = temporary.most_common(10)
 for item in a:
     print(len(item[0]))'''
 write_data(result)
-
-
-#  extract a copy of dba name column
