3a4631708238c29e90cf91620230218d3f29dca9
GsProjects <gearoidlacey@gmail.com>
2017-11-14 00:10:46 +0000

    Previously thought code was correctly parsing rows which was incorrect, this issue is corrected in this commit

--- CA 2/WebScraper.py
+++ CA 2/WebScraper.py
@@ -1,3 +1,4 @@
+import bs4
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -22,20 +23,31 @@ def parse_html(content):
             if len(row.td.attrs) > 0: #  if the td tag has one or more attributes
                 if 'rowspan' in row.td.attrs: # if theres a rowspan
                     attribute = row.td.attrs['rowspan']
-                    num_merged = int(attribute) + 1
+                    num_merged = int(attribute)
                     merged_rows = [items for items in table_rows[new_index: new_index + num_merged]]
+                    #print('MERGED ROW   1')
+                    #print(merged_rows)
+                    #print('############################################')
                     parse_rows(merged_rows)
                     merged_rows = []
-                    new_index = new_index + num_merged - 1
+                    new_index = new_index + num_merged
                     num_merged = 0
 
                 # NEED ELSE TO DEAL WITH TD tags that have a style attribute
                 else: #  if not a rowspan attribute
+                    print('############################################')
+                    print('MERGED ROW   2')
+                    print(row)
+                    print('############################################')
                     parse_rows(row)
                     new_index += 1
 
             else:
                 new_index += 1
+                print('############################################')
+                print('MERGED ROW   3')
+                print(row)
+                print('############################################')
                 parse_rows(row)
 
 
@@ -47,25 +59,51 @@ def remove_tags(data):
 
 
 def parse_rows(rows):
-    president_data =[]
+    president_rows=[]
     parsed_row=[]
-    for items in rows:
-        if len(items) > 1:
-            #check if the td tag has an embedded rowspan
 
-            table_data = items.findAll('td')
-            president_data = [item for item in table_data]
-    with open('Row.txt', 'a') as file:
-        for item in president_data[1:]:  # skip first element as it is just a column number
-            text = item.get_text()
-            text = text.replace('\n',' ')
-            text = text.replace('\n–\n',' ')
-            parsed_row.append(text)
-        file.write(str(parsed_row))
-        file.write('################################################### \n')
-        file.close()
+
+    for items in rows:
+        if len(items) >= 1:
+            if type(items) is not bs4.element.NavigableString:
+                #check if the td tag has an embedded rowspan
+
+
+
+                table_data = items.findAll('td')
+                if table_data == []:
+                    president_rows.extend(items)
+                president_data = [item for item in table_data]
+                president_rows.extend(president_data)
+
+        else:
+            if items.name == 'td':
+                #  contains all the links required for part two
+                print('')
+
+    separate_data(president_rows)
+    #president_data = []
+
 
     return True
+
+
+def separate_data(president_rows):
+    parsed_row =[]
+    with open('Row.txt', 'a') as file:
+        for item in president_rows[1:]:  # skip first element as it is just a column number
+            if type(item) is not bs4.element.NavigableString:
+                #print('ITEM:     ' , item , '\n')
+                text = item.get_text()
+                text = text.replace('\n',' ')
+                text = text.replace('\n–\n',' ')
+                parsed_row.append(text)
+        if len(parsed_row) > 0:
+            file.write(str(parsed_row))
+            file.write('###################################################')
+            file.write(str(len(parsed_row)))
+            file.write('\n')
+            file.close()
 
 
 result = get_html()
