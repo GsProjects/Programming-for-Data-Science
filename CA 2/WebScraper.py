b38dfab94dd975e115001b1f2314ef1b6ea0fbde
GsProjects <gearoidlacey@gmail.com>
2017-11-21 17:06:24 +0000

    Prepping data for database insertion

--- CA 2/WebScraper.py
+++ CA 2/WebScraper.py
@@ -1,6 +1,7 @@
 import bs4
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import datetime
 
 
 def get_html():
@@ -48,66 +49,159 @@ def remove_tags(data):
 
 
 def parse_rows(rows):
-    president_rows=[]
-    for items in rows:
+    president_rows = []
+    president_data = []
+    table_data = []
+    for items in rows: #  for each td element in the list
         if len(items) >= 1:
             if type(items) is not bs4.element.NavigableString:
-                #check if the td tag has an embedded rowspan
+                if items.name =='td':
+                    table_data.append(items) #  if the rowspan is one
 
-                table_data = items.findAll('td')
-                if table_data == []:
-                    #links = items.findAll('a', href=True)
-                    #state_links = [link for link in links if 'class' not in link.attrs and len(link.attrs) == 2 and 'Presidencey' not in link.attrs['href'] ]
-                    president_rows.extend(items)
+                else:
+                    #  if the president has multiple rows
+                    table_data.extend(items.findAll('td')) #  get all the td elements in the row
 
-                president_data = [item for item in table_data]
-                president_rows.extend(president_data)
 
-    separate_data(president_rows)
+    td_data = parse_td_data(table_data)
+    separate_data(td_data)
     return True
+
 
 
 def separate_data(president_rows):
     parsed_row =[]
-    with open('Row.txt', 'a') as file:
-        for item in president_rows[1:]: #  skip first element as its just a column number
-            if type(item) is not bs4.element.NavigableString:
-                text = item.get_text()
-                text.strip('')
-                text = text.replace('\n',' ')
-                text = text.replace('\n–\n',' ')
-                text = text.replace("\xa0",'')
-                parsed_row.append(text)
-        if len(parsed_row) > 0:
-            parse_text_data(parsed_row)
-            file.write(str(parsed_row))
-            file.write('###################################################')
-            file.write(str(len(parsed_row)))
-            file.write('\n')
-            file.close()
+    if len(president_rows) > 0:
+        with open('Row.txt', 'a') as file:
+            for item in president_rows:
+
+                if type(item) is not bs4.element.NavigableString:
+                    text = item.get_text()
+                    text.strip('')
+                    text = text.replace('\n',' ')
+                    text = text.replace('\n–\n',' ')
+                    text = text.replace("\xa0",'')
+                    parsed_row.append(text)
+            if len(parsed_row) > 0:
+                if len(parsed_row) == 5:
+                    file.write(str(parsed_row))
+                    file.write('###################################################')
+                    file.write(str(len(parsed_row)))
+                    file.write('\n')
+                    file.close()
+                prep_data(parsed_row)
+                '''file.write(str(parsed_row))
+                file.write('###################################################')
+                file.write(str(len(parsed_row)))
+                file.write('\n')
+                file.close()'''
 
 
-def parse_text_data(data):
-    if len(data) == 9:
-        temporary = data[-2]  # stores the vp name
-        data[-2] = data[-1]
-        data[-1] = temporary
-    '''if len(data) == 10:
-        print('')
-        for index,items in enumerate(data):
-            if items.count('(') == 2:
+def parse_td_data(data):
+    print('')
+    print('')
+    print('')
 
-                print(items, ' ', index)
-        second_date = data[index]
-        data[index-1]
-        print('\n')
-        #print(data)'''
-    if len(data)==15:
-        print('')
-        print(data)
+    removal_values = [0,1,2,2]
+    if len(data) > 8 :
+        for items in removal_values:
+            data.remove(data[items])
+
+        for items in data:
+            if len(items.get_text()) >= 15 and len(items.get_text()) <= 18: #  remove data from term column
+                data.remove(items)
+
+            if len(items.get_text()) >= 23 and len(items.get_text()) <= 24: #  special case for when the term data is longer than normal e.g Abraham Lincoln
+                data.remove(items)
+
+    elif len(data) <= 8:
+        removal_values = [0, 1, 2]
+
+        for items in removal_values:
+            data.remove(data[items])
+
+        for items in data:
+            if len(items.get_text()) >= 15 and len(items.get_text()) <= 18:
+                data.remove(items)
+
+            if len(items.get_text()) >= 23 and len(items.get_text()) <= 24: #  special case for when the term data is longer than normal e.g Abraham Lincoln
+                data.remove(items)
+
+    return data
+
+
+def prep_data(data):
+    parties = ['Whig','Unaffiliated','Democratic','National Union','Democratic- Republican','Federalist', 'Republican']
+    party = []
+    information =[]
+    president_name= ''
+    president_age = ''
+    presidential_status =''
+    status = ['(Died', '(Resigned']
+    print('')
+    print('')
+
+    #  split the presidency dates
+    presidency_dates = data[0].split('–') #  possible encoding error with the hyphen as the mac hyphen will not work in the split function
+
+    #  split the president information
+    president_info = []
+    for index, items in enumerate(data[1].split()):
+        if items == '(Lived:' or items == 'years)' or items == 'years' or items == 'old)' or  items == 'Born':
+            items = ''
+        if len(items) > 1: #  remove the spaces left by the previous if statement
+            if items[0] == '(':  # if the age is displayed as (93 only take everything after the (
+                items = items[1:]
+            president_info.append(items)
+
+    president_info.remove(president_info[-2]) #  remove the year the president was born and or died
+
+    for items in president_info[:-1]:
+        president_name += items + ' '
+
+    president_age = president_info[-1]
+
+    if len(data) >= 4:
+
+        #DIED IN OFFICE AND SUCCEEDED TO PRESIDENCY
+        for items in status:
+            for elements in presidency_dates[1].split():
+
+                if items == elements:
+                    presidential_status = items[1:] #  remove the bracket
+
+                    presidency_dates[1] = presidency_dates[1][:presidency_dates[1].index(items) ] # get the date up until the status
+
+
+
+        information.extend(presidency_dates)
+        information.append(presidential_status) #  set to '' at top if nothing is found
+        information.append(president_name)
+        information.append(president_age)
+        information.append(data[2])
+        information.append(data[3])
+        print(information)
+
+
+    if len(data) > 4:
+        if len( data[4]) == 16:
+            data.remove(data[4]) #  exception case where the term date is not caught
+
+
+        #print('DATA 4   :',data[4])
+        if len(data) == 6: #  if they were associated with two parties use the last one
+            party = [items for items in data[5].split() if items in parties]
+
+
+
+
+
+
+
 
 
 result = get_html()
 data = remove_tags(result)
 parse_html(data)
+
 
