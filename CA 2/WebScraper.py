4c6fda1cc503b69de230e7d9a206f3b986bb1b06
GsProjects <gearoidlacey@gmail.com>
2017-11-22 01:00:02 +0000

    Partially prepped for database insertion

--- CA 2/WebScraper.py
+++ CA 2/WebScraper.py
@@ -146,65 +146,54 @@ def prep_data(data):
     presidency_dates = data[0].split('–') #  possible encoding error with the hyphen as the mac hyphen will not work in the split function
 
     #  split the president information
-    president_info = []
-    for index, items in enumerate(data[1].split()):
-        if items == '(Lived:' or items == 'years)' or items == 'years' or items == 'old)' or  items == 'Born':
-            items = ''
-        if len(items) > 1: #  remove the spaces left by the previous if statement
-            if items[0] == '(':  # if the age is displayed as (93 only take everything after the (
-                items = items[1:]
-            president_info.append(items)
-
-    president_info.remove(president_info[-2]) #  remove the year the president was born and or died
-
-    for items in president_info[:-1]:
-        president_name += items + ' '
-
-    president_age = president_info[-1]
+    president_info = parse_president_info(data)
 
     if len(data) >= 4:
 
-        #DIED IN OFFICE
-        for items in status:
-            for elements in presidency_dates[1].split():
-
-                if items == elements:
-                    presidential_status = items[1:] #  remove the bracket
-
-                    presidency_dates[1] = presidency_dates[1][:presidency_dates[1].index(items) ] # get the date up until the status
+        #  if they died in office
+        presidency_info = presidency_information(presidency_dates)
 
 
 
-        information.extend(presidency_dates)
-        information.append(presidential_status) #  set to '' at top if nothing is found
-        information.append(president_name)
-        information.append(president_age)
-        information.append(data[2])
+
+        information.extend(presidency_info)
+        information.extend(president_info) #  set to '' at top if nothing is found
+        information.append(data[2]) #  appends the party name
+
+        #information.append(president_name)
+        #information.append(president_age)
+        #information.append(data[2])
         #  if they succeeded to presidency
-        for items in status:
-            for elements in data[3].split():
-                if items == elements:
-                    for element in data[3].split()[ : data[3].split().index(items)]:
-                        first_vp_data += element + ' '  #  remove the bracket
+        first_vp_data = vp_info(data[3],presidency_info[0], presidency_info[1])
+        information.extend(first_vp_data)
+        #print(len(data))
+        '''if len(data) > 4:
+            vp_data = vp_info(data[4], presidency_info[0], presidency_info[1])
+            information.extend(vp_data)
+            print('DATA 4: ', data[4])'''
 
-        if first_vp_data == '':
-            first_vp_data = data[3]
+        if len(data) >= 5:
+            vp_data = vp_info(data[4], presidency_info[0], presidency_info[1])
+            information.extend(vp_data)
+            print('vp_data: ', vp_data)
+            print('DATA 5: ', data[4])
 
-        for elements in first_vp_data.split():
-            if elements == '–':
-                print(first_vp_data.split())
-                hyphen_index = first_vp_data.split().index(elements)
-                if first_vp_data.split()[ hyphen_index - 3] in calendar.month_name:
-                    first_vp_name = first_vp_data.split()[ : hyphen_index - 3]
-                    print('VP NAME: ', first_vp_name)
-                elif first_vp_data.split()[ hyphen_index - 2] in calendar.month_name:
-                    first_vp_name = first_vp_data.split()[: hyphen_index - 2]
-                    print('VP NAMEs: ', first_vp_name)
-                #print('VP DATA: ', data)
+        if len(data) >= 6:
+            vp_data = vp_info(data[5], presidency_info[0], presidency_info[1])
+            information.extend(vp_data)
+            print('vp_data: ', vp_data)
+            print('DATA 6: ', data[5])
+
+        if len(data) >= 7:
+            vp_data = vp_info(data[6], presidency_info[0], presidency_info[1])
+            information.extend(vp_data)
+            print('vp_data: ', vp_data)
+            print('DATA 7: ', data[6])
 
 
-        #information.append(first_vp_data)
 
+        #information.extend(first_vp_data)
+        print(information)
 
 
     if len(data) > 4:
@@ -217,7 +206,7 @@ def prep_data(data):
             party = [items for items in data[5].split() if items in parties]
 
 
-'''def parse_president_info(data):
+def parse_president_info(data):
     president_info = []
     president_name = '' 
     new_data = []
@@ -238,7 +227,92 @@ def prep_data(data):
 
     new_data.append(president_name)
     new_data.append(president_age)
-    return new_data'''
+    return new_data
+
+def presidency_information(presidency_dates):
+
+    new_data =[]
+    status = ['(Died', '(Resigned', '(Succeeded']
+    for items in status:
+        for elements in presidency_dates[1].split():
+            if items == elements:
+                presidential_status = items[1:]  # remove the bracket
+
+                presidency_dates[1] = presidency_dates[1][ :presidency_dates[1].index(items)]  # get the date up until the status
+
+                new_data.append(presidency_dates[0])
+                new_data.append(presidency_dates[1])
+                new_data.append(presidential_status)
+    if len(new_data) == 0:
+        new_data = presidency_dates
+        new_data.append('') # add an empty field to represent status...for easier insertion in db
+    return new_data
+
+
+
+def vp_info(data, start_vacancy, end_vacancy):
+
+    status = ['(Died', '(Resigned', '(Succeeded']
+    first_vp_data = ''
+    new_data =[]
+
+    first_vp_data = data
+    if data == 'Office vacant': # if the vp is vacant for the entire presidency use presidency dates of vacancy dates
+        new_data.append(data)
+        new_data.append(start_vacancy)
+        new_data.append(end_vacancy)
+    elif len(data.split()) == 2: #  if theres only one vice president
+        new_data.append(data)
+    elif data[:13] == 'Office vacant': #  if its vacant for the balance of a term
+        new_data.append(data[:13])
+    else:
+        for elements in first_vp_data.split():
+               if elements == '–':
+                   hyphen_index = first_vp_data.split().index(elements)
+                   if first_vp_data.split()[ hyphen_index - 3] in calendar.month_name:
+                       first_vp_name = first_vp_data.split()[ : hyphen_index - 3]
+                       vp_name = ' '.join(first_vp_name)
+                       if vp_name == 'Office vacant':
+                           end_date = ' '.join(first_vp_data.split()[hyphen_index + 1: hyphen_index + 4])
+                           start_date = first_vp_data.split()[hyphen_index - 3: hyphen_index]
+                           start_date = ' '.join(start_date)
+
+                           new_data.append(vp_name)
+                           new_data.append(start_date)
+                           new_data.append(end_date)
+                       else:
+                           new_data.append(vp_name)
+
+
+
+                   elif first_vp_data.split()[ hyphen_index - 2] in calendar.month_name:
+                       first_vp_name = first_vp_data.split()[: hyphen_index - 2]
+                       vp_name = ' '.join(first_vp_name)
+                       if vp_name == 'Office vacant':
+
+                           end_date = ' '.join(first_vp_data.split()[hyphen_index + 1: hyphen_index + 4])
+                           start_date = first_vp_data.split()[hyphen_index - 2: hyphen_index]
+                           start_date.append(end_date.split()[2])
+                           start_date = ' '.join(start_date)
+                           #  if the year is missing because because they died or left in the same year then get the year from the end date
+
+                           new_data.append(vp_name)
+                           new_data.append(start_date)
+                           new_data.append(end_date)
+                       else:
+                           new_data.append(vp_name)
+
+
+
+
+    #print('NEW DATA:  ', new_data)
+    return new_data
+
+
+    # if its lenght is less than 20 return the vp name  DONE
+    # if it contains office vacant and nothing else give return a list consistin of vacant and the presidency dates -> adjust function header
+    # otherwise parse it and return the vp name
+
 
 
 
